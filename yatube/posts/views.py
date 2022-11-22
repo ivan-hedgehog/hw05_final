e7464458b63471116fa5f95d7180f0f2a68f01e3
Иван Ильин <ezikov163@gmail.com>
2022-11-22 22:27:47 +0300

    6 спринт проверка

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -118,7 +118,8 @@ def follow_index(request):
 @login_required
 def profile_follow(request, username):
     author = get_object_or_404(User, username=username)
-    if request.user != author and author != author.following:
+    if (request.user != author
+            and not request.user.follower.filter(author=author).exists()):
         Follow.objects.create(user=request.user, author=author)
     return redirect('posts:profile', username=author.username)
 
